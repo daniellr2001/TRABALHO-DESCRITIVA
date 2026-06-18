87c46e566d2c55e8c9413c0be923bc4e3d2a5dea
daniellr2001 <98185418+daniellr2001@users.noreply.github.com>
2026-06-18 14:35:10 -0300

    Update analise.R

--- analise.R
+++ analise.R
@@ -1,12 +1,10 @@
 library(ggplot2)
 library(crosstable)
-setwd("/home/daniel/Downloads/Datadecompress/archive")
 
-nomes <- read.csv("former_names.csv")
-gols <- read.csv("goalscorers.csv")
-placares <- read.csv("results.csv")
-penaltis <- read.csv("shootouts.csv")
-
+nomes <- read.csv("dados/former_names.csv")
+gols <- read.csv("dados/goalscorers.csv")
+placares <- read.csv("dados/results.csv")
+penaltis <- read.csv("dados/shootouts.csv")
 
 hist(gols$minute)
 
