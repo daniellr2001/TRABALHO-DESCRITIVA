afe044d0c320c478eefc533ac3b96eae58826532
Daniel <daniellr01@hotmail.com>
2026-06-18 14:06:21 -0300

    teste pra ver se foi

--- analise.R
+++ analise.R
@@ -1,1 +1,12 @@
+library(ggplot2)
+library(crosstable)
+setwd("/home/daniel/Downloads/Datadecompress/archive")
+
+nomes <- read.csv("former_names.csv")
+gols <- read.csv("goalscorers.csv")
+placares <- read.csv("results.csv")
+penaltis <- read.csv("shootouts.csv")
+
+
+hist(gols$minute)
 
